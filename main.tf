641051b2f2c148a3bb5510eb7d3a7ceb34c7d876
cjungm <candy7manner@gmail.com>
2020-03-04 14:07:03 +0900

    update MySQL version

--- main.tf
+++ main.tf
@@ -733,7 +733,7 @@ resource "aws_db_instance" "CRBS-rds-instance" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7.26"
+  engine_version       = "8.0.19"
   instance_class       = "db.t2.micro"
   username             = var.db_username
   password             = var.db_password
