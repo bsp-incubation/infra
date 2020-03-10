e6e3363189d5a080574063fa63c8f457bb1d628e
cjm95 <60121218+cjm95@users.noreply.github.com>
2020-03-10 13:00:46 +0900

    output resource name changed
    CRBS2->CRBS

--- output.tf
+++ output.tf
@@ -1,33 +1,33 @@
 output "vpc_id" {
-  value="${aws_vpc.CRBS2-vpc.id}"
+  value="${aws_vpc.CRBS-vpc.id}"
 }
 output "public_subnet_a_id" {
-  value="${aws_subnet.CRBS2-subnet-public-a.id}"
+  value="${aws_subnet.CRBS-subnet-public-a.id}"
 }
 output "public_subnet_c_id" {
-  value="${aws_subnet.CRBS2-subnet-public-c.id}"
+  value="${aws_subnet.CRBS-subnet-public-c.id}"
 }
 output "private_subnet_a_id" {
-  value="${aws_subnet.CRBS2-subnet-private-a.id}"
+  value="${aws_subnet.CRBS-subnet-private-a.id}"
 }
 output "private_subnet_c_id" {
-  value="${aws_subnet.CRBS2-subnet-private-c.id}"
+  value="${aws_subnet.CRBS-subnet-private-c.id}"
 }
 output "igw_id" {
   value="${aws_internet_gateway.CRBS2-igw.id}"
 }
 output "nat_id" {
-  value="${aws_nat_gateway.CRBS2-nat.id}"
+  value="${aws_nat_gateway.CRBS-nat.id}"
 }
 output "route_table_public_id" {
-  value="${aws_route_table.CRBS2-route_table-public.id}"
+  value="${aws_route_table.CRBS-route_table-public.id}"
 }
 output "route_table_private_id" {
-  value="${aws_route_table.CRBS2-route_table-private.id}"
+  value="${aws_route_table.CRBS-route_table-private.id}"
 }
 output "security_group_public_id" {
-  value="${aws_security_group.CRBS2-security_group-public.id}"
+  value="${aws_security_group.CRBS-security_group-public.id}"
 }
 output "security_group_private_id" {
-  value="${aws_security_group.CRBS2-security_group-private.id}"
+  value="${aws_security_group.CRBS-security_group-private.id}"
 }
