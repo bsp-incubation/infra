88c36eba190ec937ab27a635da7d90905c58514c
cjm95 <60121218+cjm95@users.noreply.github.com>
2020-03-12 21:59:23 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -459,25 +459,25 @@ resource "aws_security_group" "CRBS-security_group-public" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["172.16.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["172.16.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["172.16.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["172.16.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     protocol   = "icmp"
