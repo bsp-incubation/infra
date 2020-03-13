9fa77ee5a6b95963909194eacd919fa5efe75216
surim-lee <60161937+surim-lee@users.noreply.github.com>
2020-03-13 11:11:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,15 +78,15 @@ resource "aws_internet_gateway" "CRBS-igw" {
 
 # 각각의 AZ의 NAT에서 사용할 EIP를 정의한다.
 # vpc = true 항목은 EIP 생성 시 EIP의 scope를 VPC로 할지 classic으로 할지 물어봤던 옵션을 의미하는 것으로 추측된다.
-resource "aws_eip" "CRBS-eip" {
+resource "aws_eip" "CRBS-EIP" {
   vpc = true
   depends_on     = [aws_internet_gateway.CRBS-igw]
-  tags = { Name = "CRBS-eip" }
+  tags = { Name = "CRBS-EIP" }
 }
 
 # NAT Gateway
-resource "aws_nat_gateway" "CRBS-natgateway" {
-  allocation_id = aws_eip.CRBS-eip.id
+resource "aws_nat_gateway" "CRBS-nat-gateway" {
+  allocation_id = aws_eip.CRBS-EIP.id
   subnet_id     = aws_subnet.CRBS-subnet-public-a.id
   depends_on        = [aws_internet_gateway.CRBS-igw]
   tags = { Name = "CRBS-natgateway" }
