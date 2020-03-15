e06fa9c9e00ec1cd024e98be76c56f8f9a64e164
cjm95 <60121218+cjm95@users.noreply.github.com>
2020-03-15 18:34:37 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -53,7 +53,7 @@ output "CRBS2_rds_instance_address" {
 }
 
 output "CRBS2_rds_instance_id" {
-  value="${aws_db_instance.CRBS-rds-instance.id}"
+  value="${aws_db_instance.CRBS-rds-instance.arn}"
 }
 
 output "aws_codedeploy_app" {
